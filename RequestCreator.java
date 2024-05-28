3e7f03dc901d6806fe8506e6eaae7d6134020ff0
HongDiep1806 <diep.tuthi.cit21@eiu.edu.vn>
2024-05-28 13:59:47 +0700

    lab6

---
+++ RequestCreator.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Asus
+ */
+public interface RequestCreator {
+
+    public RequestProduct createRequestProduct();
+
+    public void processRequest();
+}
